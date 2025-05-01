4b6e57d7e47750879889664f484f81f96cdaaf5c
navjack <jack.mangano@gmail.com>
2025-05-01 16:29:47 -0400

    Initial Commit

---
+++ M1 StresserTests/M1_StresserTests.swift
@@ -0,0 +1,17 @@
+//
+//  M1_StresserTests.swift
+//  M1 StresserTests
+//
+//  Created by Jack Mangano on 5/1/25.
+//
+
+import Testing
+@testable import M1_Stresser
+
+struct M1_StresserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
